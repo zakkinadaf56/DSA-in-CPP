0c583799e4395676e6acc34fcebd74dbcff11541
zakkinadaf56 <zakkinadaf56@gmail.com>
2025-12-17 11:56:51 +0000

    dynamic 2d aray

---
+++ dynamic2darr.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    // int n;
+    // cin>>n;
+    int row;
+    int col;
+    cin>>row;
+    cin>>col;
+    //creating 2d dynamic array
+    int ** arr=new int *[row];
+    for (int i = 0; i < row; i++)
+    {
+        arr[i]=new int[col];
+    }
+    //taking input
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            cin>>arr[i][j];
+        }
+    }
+    
+    return 0;
+}
